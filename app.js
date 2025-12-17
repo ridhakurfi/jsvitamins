dbfa30d54011d82c7996a70a8ccc2d958a4e0fa2
ridhakurfi <ridhakurfi@gmail.com>
2025-12-17 13:14:13 +0700

    dec 17

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const Controller = require("./controllers/controller");
 
-const terminal = process.argv.slice(2);
-const command = terminal[0];
-const action1 = terminal[1];
-const action2 = terminal[2];
-const action3 = terminal[3];
-const action4 = terminal[4];
+const point = process.argv.slice(2);
+const command = point[0];
+const action1 = point[1];
+const action2 = point[2];
+const action3 = point[3];
+const action4 = point[4];
 
 switch (command) {
   case "disastersList":
